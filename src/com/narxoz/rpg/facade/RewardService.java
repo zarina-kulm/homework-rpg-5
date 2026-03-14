2e7079580f69c516c032746e4ccdcf649493ff77
Zarina <zarina@gmail.com>
2026-03-14 22:32:22 +0500

    Added RewardService class

--- src/com/narxoz/rpg/facade/RewardService.java
+++ src/com/narxoz/rpg/facade/RewardService.java
@@ -1,11 +1,12 @@
 package com.narxoz.rpg.facade;
-
 public class RewardService {
     public String determineReward(AdventureResult battleResult) {
-        // TODO: Decide reward rules based on battle outcome.
         if (battleResult == null) {
-            return "TODO";
+            return "No reward: battle result is missing.";
         }
-        return "TODO";
+        if ("Hero".equalsIgnoreCase(battleResult.getWinner())) {
+            return "100 gold + Epic Sword";
+        }
+        return "No reward";
     }
 }
