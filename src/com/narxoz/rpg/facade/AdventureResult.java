8a733b79f4124298cd04845a51dea8a0bf093c55
Zarina <zarina@gmail.com>
2026-03-14 23:03:54 +0500

    Added AdventureResult class

--- src/com/narxoz/rpg/facade/AdventureResult.java
+++ src/com/narxoz/rpg/facade/AdventureResult.java
@@ -1,9 +1,7 @@
 package com.narxoz.rpg.facade;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
 public class AdventureResult {
     private String winner;
     private int rounds;
@@ -13,31 +11,24 @@ public class AdventureResult {
     public String getWinner() {
         return winner;
     }
-
     public void setWinner(String winner) {
         this.winner = winner;
     }
-
     public int getRounds() {
         return rounds;
     }
-
     public void setRounds(int rounds) {
         this.rounds = rounds;
     }
-
     public String getReward() {
         return reward;
     }
-
     public void setReward(String reward) {
         this.reward = reward;
     }
-
     public void addLine(String line) {
         log.add(line);
     }
-
     public List<String> getLog() {
         return Collections.unmodifiableList(log);
     }
