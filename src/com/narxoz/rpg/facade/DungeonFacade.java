026d869161373f6df52139006d735f72d2f92855
Zarina <zarina@gmail.com>
2026-03-14 23:31:11 +0500

    Added DungeonFacade class

--- src/com/narxoz/rpg/facade/DungeonFacade.java
+++ src/com/narxoz/rpg/facade/DungeonFacade.java
@@ -1,9 +1,7 @@
 package com.narxoz.rpg.facade;
-
 import com.narxoz.rpg.decorator.AttackAction;
 import com.narxoz.rpg.enemy.BossEnemy;
 import com.narxoz.rpg.hero.HeroProfile;
-
 public class DungeonFacade {
     private final PreparationService preparationService = new PreparationService();
     private final BattleService battleService = new BattleService();
@@ -13,17 +11,13 @@ public class DungeonFacade {
         battleService.setRandomSeed(seed);
         return this;
     }
-
     public AdventureResult runAdventure(HeroProfile hero, BossEnemy boss, AttackAction action) {
-        // TODO: Coordinate subsystem calls in a clean order.
-        // Suggested flow:
-        // 1) preparation
-        // 2) battle
-        // 3) reward
-        AdventureResult result = battleService.battle(hero, boss, action);
         String preparationSummary = preparationService.prepare(hero, boss, action);
+        AdventureResult result = battleService.battle(hero, boss, action);
         result.addLine(preparationSummary);
-        result.setReward(rewardService.determineReward(result));
+        String reward = rewardService.determineReward(result);
+        result.setReward(reward);
+
         return result;
     }
 }
