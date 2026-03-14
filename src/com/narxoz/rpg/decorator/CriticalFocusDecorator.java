eb68cf99d3f07777a1aed26a5c9fbabff721f180
Zarina <zarina@gmail.com>
2026-03-14 18:12:05 +0500

    Added CriticalFocusDecorator class

--- src/com/narxoz/rpg/decorator/CriticalFocusDecorator.java
+++ src/com/narxoz/rpg/decorator/CriticalFocusDecorator.java
@@ -1,25 +1,20 @@
 package com.narxoz.rpg.decorator;
-
 public class CriticalFocusDecorator extends ActionDecorator {
+    private static final double CRITICAL_MULTIPLIER = 1.5;
     public CriticalFocusDecorator(AttackAction wrappedAction) {
         super(wrappedAction);
     }
 
     @Override
     public String getActionName() {
-        // TODO: Decide how this decorator changes the visible action name.
-        return super.getActionName();
+        return super.getActionName() + " [Critical Focus]";
     }
-
     @Override
     public int getDamage() {
-        // TODO: Add critical-related behavior on top of wrapped damage.
-        return super.getDamage();
+        return (int) (super.getDamage() * CRITICAL_MULTIPLIER);
     }
-
     @Override
     public String getEffectSummary() {
-        // TODO: Append or compose the critical effect description.
-        return super.getEffectSummary();
+        return super.getEffectSummary() + ", increased critical damage";
     }
 }
