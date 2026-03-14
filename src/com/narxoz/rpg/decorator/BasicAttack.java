fda189a6fee890800b940470809d8130e496e89a
Zarina <zarina@gmail.com>
2026-03-14 15:50:35 +0500

    Added BasicAttack implementation

--- src/com/narxoz/rpg/decorator/BasicAttack.java
+++ src/com/narxoz/rpg/decorator/BasicAttack.java
@@ -1,5 +1,4 @@
 package com.narxoz.rpg.decorator;
-
 public class BasicAttack implements AttackAction {
     private final String actionName;
     private final int baseDamage;
@@ -8,22 +7,16 @@ public class BasicAttack implements AttackAction {
         this.actionName = actionName;
         this.baseDamage = baseDamage;
     }
-
     @Override
     public String getActionName() {
-        // TODO: Return the base name of the action.
         return actionName;
     }
-
     @Override
     public int getDamage() {
-        // TODO: Return the base damage before decorators modify it.
         return baseDamage;
     }
-
     @Override
     public String getEffectSummary() {
-        // TODO: Decide how a plain attack should describe its effects.
-        return "TODO";
+        return "No additional effects";
     }
 }
